b5abaadc777e6854b37da00db497e9e62f857616
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2025-07-11 12:49:30 +0000

    this will be hosted at https://iamtiksha.com/dev-tools/json-editor-pro/

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/dev-tools/json-editor-pro',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
