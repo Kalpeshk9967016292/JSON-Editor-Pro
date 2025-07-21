63fc727089f1fba83a9a4a94933ae3291b0b3cd4
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2025-07-21 18:24:18 +0000

    move text div out side the group/sidebar-wrapper div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import JsonEditorPro from '@/components/json-editor-pro';
+import { JsonInfoArticle } from '@/components/json-info-article';
 
 export default function Home() {
-  return <JsonEditorPro />;
+  return (
+    <div className="flex flex-col min-h-svh">
+      <div className="flex-1 flex flex-col">
+        <JsonEditorPro />
+      </div>
+      <div className="p-4 lg:p-6">
+        <JsonInfoArticle />
+      </div>
+    </div>
+  );
 }
