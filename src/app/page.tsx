6dc259dd7c446e9ce937616b375a79e4a35aaa9b
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2025-07-21 18:26:05 +0000

    nope still same

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { JsonInfoArticle } from '@/components/json-info-article';
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="relative h-svh">
+      <div className="relative h-svh overflow-hidden">
         <JsonEditorPro />
       </div>
       <div className="p-4 lg:p-6 bg-background">
